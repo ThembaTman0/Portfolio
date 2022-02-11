7ddd55534a76b1fd31aec5af33d58c566409f15c
ThembaTman0 <thembatman0@gmail.com>
2022-02-11 15:33:08 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: [],
+  media: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'body': '#1717c26',
+        'selected-text': '#A3A3FF',
+        'theme': '#3F3FFF',
+        'nav': '#404053',
+        'secondary': '#9191A4',
+        'badge': '#3F3F51',
+        'input-border': '#565666',
+        'input': '#2A2A35',
+        'dawn': '#ff9900'
+      },
+      fontFamily: {
+        'poppins': ["'Poppins'", 'sans-serif']
+      }
+    },
+  },
+  variants: {
+    scrollbar:['rounded'],
+    extend: {},
+  },
+
+}
